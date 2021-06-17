95ee2e9b797c56fa3505407aed11d361f141c4a0
Luiz Carlos Menezes Santos <luiz.santos@paygo.com.br>
2021-06-16 21:16:18 -0300

    Schemas e transações;

--- ecommerce/src/ecommerce/core.clj
+++ ecommerce/src/ecommerce/core.clj
@@ -7,5 +7,15 @@
 (def conn (db/abre-conexao))
 (pprint conn)
 
-(pprint
- (model/novo-produto "Computador Novo", "/computador_novo", 2500.10))
+(db/cria-schema conn)
+
+(let [computador (model/novo-produto "Computador Novo", "/computador_novo", 2500.10M)]
+  (d/transact conn [computador]))
+
+(let [computador (model/novo-produto "Celular caro", "/celular", 8888.10M)]
+  (d/transact conn [computador]))
+
+(def db (d/db conn))
+
+(d/q '[:find ?entidade
+       :where [?entidade :produto/nome]] db)
